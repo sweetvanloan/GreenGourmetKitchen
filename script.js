7d64433fbf4c0792cdd401101ac04464451b73ee
Sweet Van Loan <Sweet@SweetVanLoan.com>
2023-12-17 12:12:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
         { title: 'Vegan Lasagna', description: 'A delicious layering of pasta, vegetables, and tofu cheese.' },
         { title: 'Quinoa Salad', description: 'A light, healthy salad with quinoa and fresh vegetables.' },
         { title: 'Vegan Burgers', description: 'Tasty and hearty burgers made from beans and spices.' }
-        // Adding more below
+        // Add more recipes here
     ];
 
     const recipeListElement = document.getElementById('recipe-list');
@@ -13,5 +13,25 @@ document.addEventListener('DOMContentLoaded', function() {
         recipeElement.classList.add('recipe');
         recipeElement.innerHTML = `<h2>${recipe.title}</h2><p>${recipe.description}</p>`;
         recipeListElement.appendChild(recipeElement);
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const fileInput = document.getElementById('file-input');
+    const uploadedPhotosDiv = document.getElementById('uploaded-photos');
+
+    fileInput.addEventListener('change', function(event) {
+        const files = event.target.files;
+        for (const file of files) {
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                const img = document.createElement('img');
+                img.src = e.target.result;
+                img.classList.add('uploaded-photo');
+                uploadedPhotosDiv.appendChild(img);
+            };
+            reader.readAsDataURL(file);
+        }
     });
 });
