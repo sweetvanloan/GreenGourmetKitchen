5548cca943a9ab041933d5ee125e2ed0d2ad973f
Sweet Van Loan <Sweet@SweetVanLoan.com>
2023-12-24 11:53:48 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const recipes = [
-        { title: 'Vegan Lasagna', description: 'A delicious layering of pasta, vegetables, and tofu cheese.' },
-        { title: 'Quinoa Salad', description: 'A light, healthy salad with quinoa and fresh vegetables.' },
-        { title: 'Vegan Burgers', description: 'Tasty and hearty burgers made from beans and spices.' }
-        // Additional recipes go here
-    ];
-
-    const recipeListElement = document.getElementById('recipe-list');
-
-    recipes.forEach(recipe => {
-        const recipeElement = document.createElement('div');
-        recipeElement.classList.add('recipe');
-        recipeElement.innerHTML = `<h2>${recipe.title}</h2><p>${recipe.description}</p>`;
-        recipeListElement.appendChild(recipeElement);
-    });
-});
-
+let currentItem = 0;
 
 document.addEventListener('DOMContentLoaded', function() {
-    const fileInput = document.getElementById('file-input');
-    const uploadedPhotosDiv = document.getElementById('uploaded-photos');
+    const items = document.querySelectorAll('.carousel-item');
+    const totalItems = items.length;
 
-    fileInput.addEventListener('change', function(event) {
-        const files = event.target.files;
-        for (const file of files) {
-            const reader = new FileReader();
-            reader.onload = function(e) {
-                const img = document.createElement('img');
-                img.src = e.target.result;
-                img.classList.add('uploaded-photo');
-                uploadedPhotosDiv.appendChild(img);
-            };
-            reader.readAsDataURL(file);
-        }
-    });
+    // Function to show a specific item
+    function showItem(itemIndex) {
+        items.forEach(item => item.style.display = 'none');
+        items[itemIndex].style.display = 'block';
+    }
+
+    // Function to move the carousel
+    window.moveCarousel = function(direction) {
+        currentItem = (currentItem + direction + totalItems) % totalItems;
+        showItem(currentItem);
+    };
+
+    // Auto-rotate carousel every 5 seconds
+     setInterval(() => moveCarousel(1), 5000);
+
+    // Initialize the carousel to show the first item
+    showItem(currentItem);
 });
